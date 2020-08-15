7423b2307dbad9ba0079dfa756ac20c48a4ba3d1
曾锐 <zengrui@gaosiedu.com>
2020-08-15 17:09:20 +0800

    vue-router

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import create from './utils/create'
 import store from './store'
-import router from './router'
+// import router from './router'
+import router from './krouter'
 
 Vue.config.productionTip = false
 
