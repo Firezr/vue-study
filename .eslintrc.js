3819d9974c43a65743575d258ac668308e0cfe9d
曾锐 <zengr666@163.com>
2020-10-26 15:25:15 +0800

    feat: clone vue/2612

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,29 @@
 module.exports = {
   root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
-  ],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: require.resolve('babel-eslint'),
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  env: {
+    es6: true,
+    node: true,
+    browser: true
+  },
+  plugins: [
+    "flowtype"
+  ],
+  extends: [
+    "eslint:recommended",
+    "plugin:flowtype/recommended"
+  ],
+  globals: {
+    "__WEEX__": true,
+    "WXEnvironment": true
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-console': process.env.NODE_ENV !== 'production' ? 0 : 2,
+    'no-useless-escape': 0,
+    'no-empty': 0
   }
 }
